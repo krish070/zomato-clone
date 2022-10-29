ed0187957222f4cc673f9435d2b595631ef7027f
Krishna <developerkrishna1802@gmail.com>
2022-10-29 22:06:16 +0530

    Filter Section Added

---
+++ src/components/common/filters/filterItem/filterItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./filterItem.css";
+
+const FilterItem = ({filter}) =>{
+    return <div className="filter-item cur-po">
+        {filter.icon && filter.icon}
+        <div className="filter-title">{filter.title}</div>
+    </div>
+}
+
+export default FilterItem;
--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,8 +1,42 @@
 import React from "react";
+import Filter from "../common/filters";
 import "./delivery.css";
 
+const deliveryFilters = [
+    {
+        id: 1,
+        icon: <i className="fi fi-rr-settings-sliders absolute-center"></i>,
+        title: "Filters"
+    },
+    {
+        id: 2,
+        title: "Rating: 4.0+"
+    },
+    {
+        id: 3,
+        title: "Safe and Hygienic"
+    },
+    {
+        id: 4,
+        title: "Pure Veg"
+    },
+    {
+        id: 5,
+        icon: <i className="fi fi-rr-apps-sort absolute-center"></i>,
+        title: "Delivery Time"
+    },
+    {
+        id: 6,
+        title: "Great Offers"
+    },
+];
+
 const Delivery = () =>{
-    return <div>This is Delivery</div>
+    return <div>
+        <div className="max-width">
+            <Filter filterList={deliveryFilters} />
+        </div>
+    </div>
 }
 
 export default Delivery;
